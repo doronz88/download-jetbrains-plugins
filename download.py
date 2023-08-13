8bebb0667f1a7af1b86ed29f0e5841e31ef10298
doronz <doron88@gmail.com>
2023-08-13 18:41:01 +0300

    download: move details url into a const

--- download.py
+++ download.py
@@ -1,30 +1,30 @@
-from pathlib import Path
 import xml.etree.ElementTree as ET
-from typing import Mapping
+from pathlib import Path
 
+import click
 import requests
 from plumbum import local, FG
-import click
 
 PLUGINS = [
     'izhangzhihao.rainbow.brackets',  # https://plugins.jetbrains.com/plugin/10080-rainbow-brackets
     'com.mallowigi',  # https://plugins.jetbrains.com/plugin/10044-atom-material-icons
 ]
 BUILD = '232.8660.197'
-URL = 'https://plugins.jetbrains.com/pluginManager?action=download&id={plugin_xml_id}&build={build}'
+DOWNLOAD_URL = 'https://plugins.jetbrains.com/pluginManager?action=download&id={plugin_xml_id}&build={build}'
+DETAILS_URL = 'https://plugins.jetbrains.com/plugins/list?pluginId={plugin_xml_id}'
 
 wget = local['wget']
 
 
 def get_plugin_latest_version(plugin_xml_id: str) -> str:
-    root = ET.fromstring(requests.get(f'https://plugins.jetbrains.com/plugins/list?pluginId={plugin_xml_id}').text)
+    root = ET.fromstring(requests.get(DETAILS_URL.format(plugin_xml_id=plugin_xml_id)).text)
     for node in root.iter('version'):
         return node.text
     raise Exception('failed to get latest version')
 
 
 def download(plugin_xml_id: str, output: Path, build: str = BUILD) -> None:
-    wget[URL.format(plugin_xml_id=plugin_xml_id, build=build), '-O', output, '--no-clobber'] & FG
+    wget[DOWNLOAD_URL.format(plugin_xml_id=plugin_xml_id, build=build), '-O', output, '--no-clobber'] & FG
 
 
 @click.command()
